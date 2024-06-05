092da9188f9ec4a3c70ea5f898a3a3fafb33121e
nikhil <nikhiltelase@gmail.com>
2024-06-06 05:28:48 +0900

    some updatss

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
+let snakeContainer = document.querySelector(".snkae-container");
 let segments = document.querySelectorAll(".snake");
 let bgAudio = document.getElementById("bg-sound");
 let eatAudio = document.getElementById("eat-sound");
 let turnAudio = document.getElementById("turn-sound");
 let overAudio = document.getElementById("over-sound");
+let gameOver = document.querySelector(".game-over");
 let guidence = document.querySelector(".guidence");
+let scoreElement = document.querySelector(".score");
+
+bgAudio.volume = 0.2;
+overAudio.volume = 0.4;
 guidence.textContent = "Press Enter To Start Game"
 
-let startingPostion = [
-    {l:18, t:0},
-    {l:15, t:0},
-    {l:12, t:0},
-    {l:9, t:0},
-    {l:6, t:0},
-    {l:3, t:0},
-];
-//create snake
-startingPostion.forEach((postion, index) => {
-    let l = postion.l;
-    let t = postion.t;
-    let segment = segments[index]
-    segment.style.left = `${l}px`;
-    segment.style.top = `${t}px`;
-})
+let score = 0;
+let playAgain = false;
+let start = true;
+
 //move snake head
 let head = segments[0];
 let topPostion = 0;
-let leftPostion = 18;
+let leftPostion = 0;
 let snakeDirection = "right";
 let step = 10;
 
@@ -34,31 +28,25 @@ function moveHead(direction){
         case "left":
             leftPostion -= step;
             break;
-
         case "right":
             leftPostion += step;
             break;
-
         case "up":
             topPostion -= step;
             break;
-
         case "down":
             topPostion += step;
             break;
-            
         default:
             break;
     }
-
     head.style.left = `${leftPostion}px`;
     head.style.top = `${topPostion}px`;
 }
+
 //move snake body
 function moveBody(){
-    // get currnet postion
     let currnetSegments = document.querySelectorAll(".snake");
-
     let index = currnetSegments.length-1;
     //index mean length of currnetSegments to ....3, 2, 1, 0 
     while (index > 0) {
@@ -67,70 +55,6 @@ function moveBody(){
         currnetSegments[index].style.left = newL;
         currnetSegments[index].style.top = newT;
         index -= 1;
-        
-    }
-}
-//extend snake
-function extendSnake(){
-    // get currnet postion
-    let snakeContainer = document.querySelector(".snkae-container");
-    let currnetSegments = document.querySelectorAll(".snake");
-    let lastSegment = currnetSegments[currnetSegments.length-1];
-
-    let newSegment = document.createElement("div");
-    
-    //new segment go to last segment position
-    newSegment.style.left = lastSegment.style.left;
-    newSegment.style.top = lastSegment.style.top;
-
-    newSegment.classList.add("snake");
-    newSegment.textContent = "🟢";
-    snakeContainer.appendChild(newSegment);
-}
-
-// crate food
-let foodLeft ;
-let foodTop ;
-function refreshFood(){
-    let foodArray = ["🍎", "🍍", "🍉", "🍅", "🍑", "🍓", "🥝", "🍒", "🍈", "🍇", "🍋", "🥕", "🍏", "🍐"]
-    let randomIndex = Math.floor(Math.random() * foodArray.length);
-
-    let food = document.querySelector(".food");
-    food.textContent = foodArray[randomIndex];
-    let randomLeft = Math.floor(Math.random() * 308) ;
-    let randomTop = Math.floor(Math.random() * 408) ;
-    food.style.left = `${randomLeft}px`;
-    food.style.top = `${randomTop}px`;
-
-    foodLeft = randomLeft;
-    foodTop = randomTop;
-}
-refreshFood();
-
-//detect collitsion with snake tail
-function detect(){
-    // get currnet postion
-    let currnetSegments = document.querySelectorAll(".snake");
-
-    let i = 6;
-    while (i < currnetSegments.length) {
-        // let bodyLeftPosition  = Number(currnetSegments[i].style.left.replace("px", ""));
-        // let leftDistence = bodyLeftPosition - leftPostion;
-        // let numLeftDistence = Number(String(leftDistence).replace("-", ""));
-
-        // let bodyTopPosition  = Number(currnetSegments[i].style.top.replace("px", ""));
-        // let topDistence = bodyTopPosition -topPostion;
-        // let numtopDistence = Number(String(topDistence).replace("-", ""));
-
-        // if (numLeftDistence == 2 && numtopDistence == 2){
-        //     return true
-        // }
-        if (head.style.left == currnetSegments[i].style.left && head.style.top == currnetSegments[i].style.top){  
-            return true
-        }
-        
-
-        i += 1;
     }
 }
 
@@ -164,20 +88,157 @@ function moveRight(){
     }
 }
 
-//change snake dirction when keybord arrow button press
-let playAgain = false;
+//extend snake
+function extendSnake(){
+    let currnetSegments = document.querySelectorAll(".snake");
+    let lastSegment = currnetSegments[currnetSegments.length-1];
+    let newSegment = document.createElement("div");
+    //new segment go to last segment position
+    newSegment.style.left = lastSegment.style.left;
+    newSegment.style.top = lastSegment.style.top;
+    newSegment.classList.add("snake");
+    newSegment.textContent = "🟢";
+    snakeContainer.appendChild(newSegment);
+}
+
+// crate food
+let foodLeft ;
+let foodTop ;
+function refreshFood(){
+    let foodArray = ["🍎", "🍍", "🍉", "🍅", "🍑", "🍓", "🥝", "🍒", "🍈", "🍇", "🍋", "🥕", "🍏", "🍐"]
+    let randomIndex = Math.floor(Math.random() * foodArray.length);
+    let food = document.querySelector(".food");
+    foodTop = Math.floor(Math.random() * 408) ;
+    foodLeft = Math.floor(Math.random() * 308) ;
+    food.textContent = foodArray[randomIndex];
+    food.style.left = `${foodLeft}px`;
+    food.style.top = `${foodTop}px`;
+}
+refreshFood();
+
+//detect collitsion with snake body
+function detectBody(){
+    let currnetSegments = document.querySelectorAll(".snake");
+
+    for (let i = 2; i < currnetSegments.length; i++) {
+        // let bodyLeftPosition  = Number(currnetSegments[i].style.left.replace("px", ""));
+        // let leftDistence = bodyLeftPosition - leftPostion;
+        // let numLeftDistence = Number(String(leftDistence).replace("-", ""));
+
+        // let bodyTopPosition  = Number(currnetSegments[i].style.top.replace("px", ""));
+        // let topDistence = bodyTopPosition -topPostion;
+        // let numtopDistence = Number(String(topDistence).replace("-", ""));
+
+        // if (numLeftDistence == 2 && numtopDistence == 2){
+        //     return true
+        // }
+        if (head.style.left == currnetSegments[i].style.left && head.style.top == currnetSegments[i].style.top){  
+            return true
+        }  
+    }
+}
+
+//start game
+function startGame(){
+    // bgAudio.play();
+    let startGame = setInterval(() => {
+    moveHead(snakeDirection);
+    moveBody();
+    let topDistence = foodTop - topPostion;
+    let leftDistence = foodLeft - leftPostion;
+    let numTopDistence = Number(String(topDistence).replace("-", ""));
+    let numLeftDistence = Number(String(leftDistence).replace("-", ""));
+    // detect collistion with food
+    if (numTopDistence < 15 && numLeftDistence < 15 ){
+        refreshFood();
+        extendSnake();   
+        score += 1;
+        scoreElement.textContent = `Score: ${score}`; 
+        // eatAudio.play()
+    }
+    //detect collition with wall
+    if (topPostion >= 425|| topPostion < -5 || leftPostion >= 332|| leftPostion < -5){
+        gameOver.style.display = "flex";
+        guidence.textContent = "Press Sift To Play Again.."
+        playAgain = true;
+        clearInterval(startGame);
+        bgAudio.pause();
+        // overAudio.play();
+        return
+    }
+    //detect collition with body
+    if (detectBody()){
+        gameOver.style.display = "flex";
+        playAgain = true;
+        clearInterval(startGame);
+        bgAudio.pause();
+        // overAudio.play();
+        return
+    }
+}, 100);
+}
+
+//play again
+function resetSnake(){
+    snakeContainer.innerHTML = `
+    <div class="snake head">🔴</div>
+    <div class="snake">🔴</div>
+    <div class="snake">🟢</div>
+    <div class="snake">🟢</div>
+    <div class="snake">🟢</div>
+    <div class="snake">🟢</div>
+    <div class="snake">🟢</div>
+    `;
+    let currnetSegments = document.querySelectorAll(".snake");
+    head = currnetSegments[0];
+    head.style.top = "0px";
+    head.style.left = "0px";
+    topPostion = 0;
+    leftPostion = 0;
+    snakeDirection = "right";
+    moveBody();
+    refreshFood();
+    gameOver.style.display = "none";
+    score = 0;
+    scoreElement.textContent = `Score: ${score}`; 
+    start = true;
+    playAgain = false;
+}
+
+//start button
+let startGameButton = document.getElementById("startGame");
+startGameButton.addEventListener("click", () => {
+    if (start){
+        console.log("start")
+        startGame()
+        start = false
+    }
+})
+
+//play agian button
+let playAgainButton = document.getElementById("playAgain");
+playAgainButton.addEventListener("click", () => {
+    resetSnake();
+});
+
+
+//keybord controls for pc or laptop
 window.addEventListener('keydown', (event) => {
     // console.log('Key pressed:', event.key); // Log any key press
 
     switch (event.key) {
         case "Shift":
             if (playAgain){
-                location.reload()
+                resetSnake();
             }
             break
 
         case "Enter":
-            startGame();
+            if (start){
+                console.log("start")
+                startGame()
+                start = false
+            }
             break
 
         case 'ArrowUp': 
@@ -200,67 +261,3 @@ window.addEventListener('keydown', (event) => {
             return;
     }
 });
-
-let gameOver = document.querySelector(".game-over");
-function startGame(){
-    // bgAudio.play();
-    bgAudio.volume = 0.2;
-    overAudio.volume = 0.4;
-    let scoreElement = document.querySelector(".score");
-    let score = 0;
-    //game loop
-    let startGame = setInterval(() => {
-    moveHead(snakeDirection);
-    moveBody();
-
-    let topDistence = foodTop - topPostion;
-    let leftDistence = foodLeft - leftPostion;
-    let numTopDistence = Number(String(topDistence).replace("-", ""));
-    let numLeftDistence = Number(String(leftDistence).replace("-", ""));
-
-    // detect collistion with food
-    if (numTopDistence < 15 && numLeftDistence < 15 ){
-        refreshFood();
-        extendSnake();   
-
-        //update secore 
-        score += 1;
-        scoreElement.textContent = `Score: ${score}`; 
-        // eatAudio.play()
-    }
-    //detect collition with wall
-    if (topPostion >= 425|| topPostion < -5 || leftPostion >= 332|| leftPostion < -5){
-        gameOver.style.display = "flex";
-        guidence.textContent = "Press Sift To Play Again.."
-        playAgain = true;
-        clearInterval(startGame);``
-        bgAudio.pause();
-        // overAudio.play();
-    }
-    //detect collition with tail
-    if (detect()){
-        gameOver.style.display = "flex";
-        playAgain = true;
-        clearInterval(startGame);
-        bgAudio.pause();
-        // overAudio.play();
-    }
-
-}, 100);
-
-}
-//start game
-let startGameButton = document.getElementById("startGame");
-let start = false;
-startGameButton.addEventListener("click", () => {
-    if (!start){
-        startGame()
-        start = true
-    }
-})
-
-// gameOver()
-gameOver.addEventListener("click", () => {
-    location.reload();
-});
-
